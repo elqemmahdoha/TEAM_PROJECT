caf2560485cf32fb136f971db5ec8c4cd05b0604
Laura CLETZ <laura@cletz.fr>
2024-11-27 11:57:03 +0100

    test

--- modules/interactive_map.py
+++ modules/interactive_map.py
@@ -1,40 +1,15 @@
-import geopandas as gpd
+import time
+start = time.time()
 import pandas as pd
 import folium
 import json
-from folium.plugins import HeatMap
-from folium.plugins import GroupedLayerControl
+from folium.plugins import HeatMap, GroupedLayerControl
 import branca.colormap as cm
 pd.options.mode.chained_assignment = None
 
 
-sf = gpd.read_file("data/openstreetdata/contour-du-departement.geojson")
-centre = [43.62505, 3.862038]
-Montpellier = folium.Map(location=centre, zoom_start=6.5, tiles="OpenStreetMap")
+Montpellier = folium.Map(location=[43.62505, 3.862038], zoom_start=10.5, tiles=None)
 
-
-folium.GeoJson(
-    sf[["geometry"]],
-    zoom_on_click=True,
-    style_function=lambda feature: {
-        "fillColor": "#003322",
-        "color": "grey",
-        "weight": 2,
-        "dashArray": "5, 5",
-        "fillOpacity": 0.01,
-    }
-).add_to(Montpellier)
-
-
-color_mapa = cm.LinearColormap(
-    colors=["red", "orange", "yellow"],
-    caption="Prédiction du trafic cycliste à Montpellier",
-)
-svg_style = "<style>svg#legend {background-color: rgba(255,255,255,0.5);}</style>"
-Montpellier.get_root().header.add_child(folium.Element(svg_style))
-color_mapa.add_to(Montpellier)
-
-grad = {0: "#0d0887", 0.5: "#6a00a8", 0.9: "#fcce25", 1: "#f0f921"}
 
 weeklyd = pd.read_csv("data/donnees_hebdo.csv", sep=";", na_values="Null", low_memory=False)
 weeklyd = pd.melt(weeklyd, id_vars=["N° Série", "Latitude", "Longitude"], 
@@ -46,53 +21,72 @@ compteurs_todrop = weeklyd[(weeklyd["N° Série"] == "X2H22104765") |
 weeklyd.drop(compteurs_todrop, inplace=True)
 weeklyd = weeklyd.groupby(["Jour", "Latitude", "Longitude"])["Intensité"].mean().reset_index(name="Intensité")
 
+
+grad = {0: "#ffff00", 0.5: "#ffcc66", 0.8: "#aa00aa", 1: "#800080"}
+color_mapa = cm.LinearColormap(
+    colors=list(grad.values()),  
+    vmin=0, vmax=weeklyd["Intensité"].max(), 
+    caption="Prédiction du trafic cycliste à Montpellier",
+)
+svg_style = "<style>svg#legend {background-color: rgba(255,255,255,0.5);}</style>"
+Montpellier.get_root().header.add_child(folium.Element(svg_style))
+color_mapa.add_to(Montpellier)
+
 days = ["18/3", "19/3", "20/3", "21/3", "22/3", "23/3", "24/3"]
 day_names = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-day_feature_groups = []
 
+day_feature_groups = []
 for day, day_name in zip(days, day_names):
     data = weeklyd[weeklyd["Jour"] == day]
+    data["Intensité"] *= 10
     heat_data = data[["Latitude", "Longitude", "Intensité"]].values.tolist()
-    
     feature_group = folium.FeatureGroup(name=day_name, show=True)
     heat_map = HeatMap(
         heat_data,
         gradient=grad,
-        radius=10,
-        blur=35,
+        radius=30,  
+        blur=15,    
     )
     feature_group.add_child(heat_map)
     Montpellier.add_child(feature_group)
     
     day_feature_groups.append(feature_group)
 
-'''
-velo_orange = images/logo_velo_orange.png
-icon = folium.CustomIcon(
-    velo_orange,
-    icon_size=(2, 2),
-    icon_anchor=(2, 2),
-    popup_anchor=(2, 2)
-)
 
-with open('data/openstreetdata/MMM_MMM_Velomagg.json',) as f:
+with open('data/openstreetdata/MMM_MMM_Velomagg.json') as f:
     velomagg_geoloc = json.load(f)
 
-for i in velomagg_geoloc["features"]:
-    folium.Marker(location=velomagg_geoloc[[["coordinates"]]], 
-            icon=velo_orange, 
-            popup=velomagg_geoloc[[["nom"]]]).add_to(Montpellier)
-'''
+for feature in velomagg_geoloc["features"]:
+    lon, lat = feature["geometry"]["coordinates"]
+    name = feature["properties"].get("nom", "Station Velomagg")
 
-folium.LayerControl(position="topleft", collapsed=True, opacity=0.7).add_to(Montpellier)
+    folium.Marker(
+        location=[lat, lon], 
+        icon=folium.Icon(icon="bicycle", 
+                    prefix="fa", icon_color="black", 
+                    color="black", icon_size=(10, 10), 
+                    shadow_size=(0,0)), 
+        popup=folium.Popup(name, parse_html=True), 
+    ).add_to(Montpellier)
+
+
 GroupedLayerControl(
     position="topleft",
     groups={"Jours de la semaine": day_feature_groups},
     collapsed=False,
 ).add_to(Montpellier)
 
-#Montpellier
-#TO_REMOVE
-Montpellier.save("mtp_interactive_test.html")
-import webbrowser
-webbrowser.open("mtp_interactive_test.html")
+folium.TileLayer("OpenStreetMap", name="Street Map").add_to(Montpellier)
+folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Satellite',
+        overlay = False,
+        control = True).add_to(Montpellier)
+folium.LayerControl(position="topleft", collapsed=False, opacity=0.7).add_to(Montpellier)
+
+
+
+Montpellier.save("mtp_interactive.html")
+end = time.time()
+print(f"Temps : {end - start:.5f} s")
