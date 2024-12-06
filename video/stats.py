c80e92381d531fa54f1256c7de236376ae31a806
Louison Gillet <louison.gillet@etu.umontpellier.fr>
2024-12-06 17:08:35 +0100

    Stats.py complete + ajout figures en .html

--- video/stats.py
+++ video/stats.py
@@ -13,6 +13,7 @@ os.makedirs(dossier_destination, exist_ok=True)  # Crée le dossier s'il n'exist
 
 # Chemins 
 chemin_average_intensity = os.path.join(dossier_destination, "average_intensity_month.html")
+chemin_polar_figure = os.path.join(dossier_destination, "polar_figure.html")
 
 # Charger le fichier GeoJSON contenant les points de comptage
 compteurs = gpd.read_file("data/video/ecocompt/GeolocCompteurs.geojson")
@@ -71,16 +72,23 @@ for idx, row in compteurs.iterrows():
         except Exception as e:
             print(f"Erreur inconnue pour le compteur {numero_serie}: {e}")
 
+# Sélectionner les colonnes qui contiennent "mean_" dans leur nom
+mean_columns = [col for col in compteurs.columns if col.startswith("mean_")] 
+
+# Calculer la moyenne pour chaque ligne, en ignorant les valeurs NaN
+compteurs['mean_total'] = compteurs[mean_columns].mean(axis=1)
+ 
 # Gestion pb Gerhardt 
 # Identifier la ligne correspondant à "Compteur Vélo Gerhardt"
 ligne_gerhardt = compteurs["Nom du com"] == "Compteur Vélo Gerhardt"
-
 # Mettre toutes les colonnes des moyennes d'intensités à None pour cette ligne
-mois_colonnes_moyenne = [col for col in compteurs.columns if col.startswith("mean_")]
-compteurs.loc[ligne_gerhardt, mois_colonnes_moyenne] = None
+compteurs.loc[ligne_gerhardt, mean_columns] = None
+
+# Calculer la moyenne de chaque colonne en ignorant les NaN
+mean_values = compteurs[mean_columns].apply(pd.to_numeric, errors='coerce').mean() # numeric pour ne pas traiter les None -> Nan 
 
 # Créer un dictionnaire pour mapper les colonnes à des noms de mois
-month_mapping = {
+month_names = {
     "mean_03": "Mars",
     "mean_04": "Avril",
     "mean_05": "Mai",
@@ -91,32 +99,58 @@ month_mapping = {
     "mean_10": "Octobre"
 }
 
-# Sélectionner les colonnes qui contiennent "mean_" dans leur nom
-mean_columns = [col for col in compteurs.columns if col.startswith("mean_")]
-
-# Calculer la moyenne de chaque colonne en ignorant les NaN
-mean_values = compteurs[mean_columns].apply(pd.to_numeric, errors='coerce').mean() # numeric pour ne pas traiter les None -> Nan 
-
 # Créer un nouveau DataFrame pour afficher les moyennes avec noms de mois
 tableau_moyennes = pd.DataFrame({
-    "Month": [month_mapping[col] for col in mean_columns],  # Utiliser les noms de mois
+    "Month": [month_names[col] for col in mean_columns],  # Utiliser les noms de mois
     "Average Intensity": mean_values.values                # Valeurs des moyennes
 })
 
-# Créer le graphique avec Plotly
-fig = px.line(tableau_moyennes, x='Month', y='Average Intensity', 
+# Trier le DataFrame par 'mean_total' de manière décroissante et sélectionner les k premiers
+k = 10
+topcompteurs = compteurs[['Nom du com', 'mean_total'] + mean_columns].sort_values(by='mean_total', ascending=False).head(k)
+topcompteurs.rename(columns=month_names, inplace=True)
+
+# Préparer les données pour la visualisation polaire
+# Transformer les moyennes mensuelles pour qu'elles soient dans un format adapté
+polar_data = topcompteurs.melt(id_vars=['Nom du com'], value_vars=list(month_names.values()), 
+                               var_name='Month', value_name='Intensity')
+
+
+if os.path.exists(chemin_polar_figure):
+    print(f"Le fichier {chemin_polar_figure} existe déjà.")
+else : 
+    # Créer une figure polaire
+    fig = px.line_polar(
+    polar_data, 
+    r='Intensity', 
+    theta='Month', 
+    color='Nom du com',
+    line_close=True,
+    range_r=[0,2500],
+    start_angle=0,
+    title="Intensité moyenne des compteurs par mois")
+
+    # Enregistrement de la figure 
+    fig.write_html(chemin_polar_figure)
+
+
+if os.path.exists(chemin_average_intensity):
+    print(f"Le fichier {chemin_average_intensity} existe déjà.")
+else:
+    # Créer le graphique avec Plotly
+    fig = px.line(tableau_moyennes, x='Month', y='Average Intensity', 
               markers=True, title="Passage moyen devant les éco-compteurs par mois sur l'année 2024")
 
-# Personnaliser la couleur de la ligne et des axes
-fig.update_traces(line=dict(color='darkviolet'))  # Couleur de la ligne en violet foncé
+    # Personnaliser la couleur de la ligne et des axes
+    fig.update_traces(line=dict(color='darkviolet'))  # Couleur de la ligne en violet foncé
 
-# Personnaliser les axes et le fond
-fig.update_layout(
-    xaxis_title="Mois",
-    yaxis_title="Intensité de passage moyenne",
-    template="plotly_white",  # Style léger avec fond blanc
-    showlegend=True
-)
+    # Personnaliser les axes et le fond
+    fig.update_layout(
+        xaxis_title="Mois",
+        yaxis_title="Intensité de passage moyenne",
+        template="plotly_white",  # Style léger avec fond blanc
+        showlegend=True
+    )
 
-# Enregistrement de la figure 
-fig.write_html(chemin_average_intensity)
+    # Enregistrement de la figure 
+    fig.write_html(chemin_average_intensity)
